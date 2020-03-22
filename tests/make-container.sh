b53e006391a60cd69177f637e47772ebdd3c369f
Kevin P. Fleming <kevin@km6g.us>
2020-03-22 17:44:54 -0400

    Begin adding test infrastructure
    'make-container.sh' builds an OCI image which can be used for testing
    this tool; it has Python 3, PowerDNS Auth Server 4.3, Ansible, and
    Bravado.
    
    Signed-off-by: Kevin P. Fleming <kevin@km6g.us>

---
+++ tests/make-container.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env -S bash -xe
+
+image=docker.io/kpfleming/apaz-test-images:pdns4.3-python3
+
+c=$(buildah from debian:buster)
+
+buildah run ${c} -- apt update
+buildah run ${c} -- apt install --yes gnupg python3 python3-venv
+
+buildah add ${c} apt-repo-pdns-auth-43.list /etc/apt/sources.list.d
+buildah add ${c} apt-pref-pdns /etc/apt/preferences.d
+curl https://repo.powerdns.com/FD380FBB-pub.asc | buildah run ${c} -- apt-key add
+buildah run ${c} -- apt update
+buildah run ${c} -- apt install --yes pdns-server
+
+buildah run ${c} -- python3 -m venv /ansible
+buildah run ${c} -- /ansible/bin/pip install wheel
+buildah run ${c} -- /ansible/bin/pip install ansible
+buildah run ${c} -- /ansible/bin/pip install bravado
+
+buildah run ${c} -- rm -rf /var/lib/apt/lists/*
+buildah run ${c} -- rm -rf /root/.cache
+
+buildah rmi ${image}
+buildah commit --squash --rm ${c} ${image}
