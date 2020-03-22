92d55cdc820da7223c350c647ea73534db5af211
Kevin P. Fleming <kevin@km6g.us>
2020-03-22 18:04:58 -0400

    Improvements to container-building script
    * Install Git
    
    * Allow script to be run from any directory
    
    Signed-off-by: Kevin P. Fleming <kevin@km6g.us>

--- tests/make-container.sh
+++ tests/make-container.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env -S bash -xe
 
+root=$(dirname ${BASH_SOURCE[0]})
+
 image=docker.io/kpfleming/apaz-test-images:pdns4.3-python3
 
 c=$(buildah from debian:buster)
 
 buildah run ${c} -- apt update
-buildah run ${c} -- apt install --yes gnupg python3 python3-venv
+buildah run ${c} -- apt install --yes gnupg python3 python3-venv git
 
-buildah add ${c} apt-repo-pdns-auth-43.list /etc/apt/sources.list.d
-buildah add ${c} apt-pref-pdns /etc/apt/preferences.d
+buildah add ${c} ${root}/apt-repo-pdns-auth-43.list /etc/apt/sources.list.d
+buildah add ${c} ${root}/apt-pref-pdns /etc/apt/preferences.d
 curl https://repo.powerdns.com/FD380FBB-pub.asc | buildah run ${c} -- apt-key add
 buildah run ${c} -- apt update
 buildah run ${c} -- apt install --yes pdns-server
