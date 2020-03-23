5038ec683c32dad0dd568018089c4a8791ead51a
Kevin P. Fleming <kevin@km6g.us>
2020-03-23 19:37:25 -0400

    Use apt-get instead apt as it has a stable CLI interface
    Signed-off-by: Kevin P. Fleming <kevin@km6g.us>

--- tests/make_container.sh
+++ tests/make_container.sh
@@ -8,15 +8,15 @@ image=quay.io/kpfleming/apaz-test-images:pdns4.3-python3
 
 c=$(buildah from debian:buster)
 
-buildah run ${c} -- apt update
-buildah run ${c} -- apt install --yes --quiet=2 gnupg python3 python3-venv git sqlite3
+buildah run ${c} -- apt-get update
+buildah run ${c} -- apt-get install --yes --quiet=2 gnupg python3 python3-venv git sqlite3
 
 buildah add ${c} ${root}/apt-repo-pdns-auth-43.list /etc/apt/sources.list.d
 buildah add ${c} ${root}/apt-pref-pdns /etc/apt/preferences.d
 curl https://repo.powerdns.com/FD380FBB-pub.asc | buildah run ${c} -- apt-key add
-buildah run ${c} -- apt update --quiet=2
-buildah run ${c} -- apt install --yes --quiet=2 pdns-server pdns-backend-sqlite3
-buildah run ${c} -- apt purge --yes --quiet=2 pdns-backend-bind
+buildah run ${c} -- apt-get update --quiet=2
+buildah run ${c} -- apt-get install --yes --quiet=2 pdns-server pdns-backend-sqlite3
+buildah run ${c} -- apt-get purge --yes --quiet=2 pdns-backend-bind
 buildah run ${c} -- sqlite3 /run/pdns.sqlite3 '.read /usr/share/doc/pdns-backend-sqlite3/schema.sqlite3.sql'
 
 buildah run ${c} -- python3 -m venv /ansible
