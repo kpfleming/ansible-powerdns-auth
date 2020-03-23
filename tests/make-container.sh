36c2eb92d15843dd3895b28780b179786f9f2776
Kevin P. Fleming <kevin@km6g.us>
2020-03-23 07:17:27 -0400

    Switch to SQlite3 backend for pdns-server
    Signed-off-by: Kevin P. Fleming <kevin@km6g.us>

--- tests/make-container.sh
+++ tests/make-container.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S bash -xe
+#!/usr/bin/env -S bash -e
 
 root=$(dirname ${BASH_SOURCE[0]})
 
@@ -7,13 +7,15 @@ image=quay.io/kpfleming/apaz-test-images:pdns4.3-python3
 c=$(buildah from debian:buster)
 
 buildah run ${c} -- apt update
-buildah run ${c} -- apt install --yes gnupg python3 python3-venv git
+buildah run ${c} -- apt install --yes gnupg python3 python3-venv git sqlite3
 
 buildah add ${c} ${root}/apt-repo-pdns-auth-43.list /etc/apt/sources.list.d
 buildah add ${c} ${root}/apt-pref-pdns /etc/apt/preferences.d
 curl https://repo.powerdns.com/FD380FBB-pub.asc | buildah run ${c} -- apt-key add
 buildah run ${c} -- apt update
-buildah run ${c} -- apt install --yes pdns-server
+buildah run ${c} -- apt install --yes pdns-server pdns-backend-sqlite3
+buildah run ${c} -- apt purge --yes pdns-backend-bind
+buildah run ${c} -- sqlite3 /run/pdns.sqlite3 '.read /usr/share/doc/pdns-backend-sqlite3/schema.sqlite3.sql'
 
 buildah run ${c} -- python3 -m venv /ansible
 buildah run ${c} -- /ansible/bin/pip install wheel
@@ -23,7 +25,7 @@ buildah run ${c} -- /ansible/bin/pip install bravado
 buildah run ${c} -- rm -rf /var/lib/apt/lists/*
 buildah run ${c} -- rm -rf /root/.cache
 
-if [ buildah image --quiet ${image} ]; then
+if buildah images --quiet ${image}; then
     buildah rmi ${image}
 fi
 buildah commit --squash --rm ${c} ${image}
--- tests/start_server.sh
+++ tests/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec pdns_server --no-config --daemon --api --api-key=foo --disable-syslog --local-port=55353 --launch=random --socket-dir=/run
+exec pdns_server --no-config --daemon --api --api-key=foo --disable-syslog --local-port=55353 --socket-dir=/run --launch=gsqlite3 --gsqlite3-database=/run/pdns.sqlite3 --gsqlite3-dnssec=on
