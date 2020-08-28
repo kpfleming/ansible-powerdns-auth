c7c703c6d1352411e23a372256da1e1ebfac1359
Kevin P. Fleming <kevin@km6g.us>
2020-08-28 15:11:51 -0400

    Update Pythons to 3.6.12 and 3.7.9

--- tests/make_tox_container.sh
+++ tests/make_tox_container.sh
@@ -20,7 +20,7 @@ buildcmd apt-get update
 buildcmd apt-get install --yes --quiet=2 git
 
 buildcmd apt-get install --yes --quiet=2 ${pydeps[@]}
-for pyver in 3.6.11 3.7.8 3.8.5; do
+for pyver in 3.6.12 3.7.9 3.8.5; do
     wget -O - https://www.python.org/ftp/python/${pyver}/Python-${pyver}.tgz | buildcmd tar xzf -
     buildah config --workingdir /root/Python-${pyver} ${c}
     buildcmd ./configure --disable-shared
