a0708ee61fac34f9d7f68c37f1a24f3558d1a580
Kevin P. Fleming <kevin@km6g.us>
2020-10-02 08:57:01 -0400

    Yet another Actions fix...
    Signed-off-by: Kevin P. Fleming <kevin@km6g.us>

--- tests/make_tox_container.sh
+++ tests/make_tox_container.sh
@@ -5,7 +5,7 @@ set -ex
 sudo apt-get update
 sudo apt-get --yes upgrade
 
-root=$(dirname ${BASH_SOURCE[0]})
+root=$(realpath $(dirname ${BASH_SOURCE[0]}))
 pdns=${1}
 pydeps=(build-essential libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev)
 
