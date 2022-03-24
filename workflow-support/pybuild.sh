87027ead7f7b8b682efd4646bc1f89ed106e5eea
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-24 19:26:55 +0800

    Delete workflow-support directory

--- workflow-support/pybuild.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo starting Python build in "${1}"
-
-set -x
-
-cd "${1}" || exit
-./configure --disable-shared
-make -j2 altinstall
--- workflow-support/start_server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-exec pdns_server --no-config --daemon --api --api-key=foo --disable-syslog --local-port=55353 --socket-dir=/run --launch=gsqlite3 --gsqlite3-database=/run/pdns.sqlite3 --gsqlite3-dnssec=on
