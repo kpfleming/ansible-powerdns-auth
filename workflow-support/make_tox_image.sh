2ff8bb8b2ccba044bc3284103b31a5652db5f4c7
Kevin P. Fleming <kevin@km6g.us>
2022-07-11 14:40:33 -0400

    Add testing against Python 3.11 beta 4

--- workflow-support/make_tox_image.sh
+++ workflow-support/make_tox_image.sh
@@ -6,7 +6,7 @@ scriptdir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 export pdns="${1}"
 lintdeps=(shellcheck)
 pydeps=(build-essential libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev)
-pyversions=(3.8.13 3.9.13 3.10.5 3.11.0b2)
+pyversions=(3.8.13 3.9.13 3.10.5 3.11.0b4)
 
 c=$(buildah from debian:buster)
 
