ec32cd62753ed99bb3bc2f327bd0f6f9ef51840e
Kevin P. Fleming <kevin@km6g.us>
2025-11-14 06:56:46 -0500

    WIP

---
+++ src/plugins/module_utils/dns_helpers.py
@@ -0,0 +1,20 @@
+# SPDX-FileCopyrightText: 2025 Kevin P. Fleming <kevin@km6g.us>
+# SPDX-License-Identifier: Apache-2.0
+# -*- coding: utf-8 -*-
+
+import dns.exception
+import dns.name
+
+
+class DNSNameError(Exception):
+    """The supplied DNS name was not valid."""
+
+    def __init__(self, name, e):
+        super().__init__(f"Invalid DNS name: {name} - {e}")
+
+
+def validate_dns_name(name):
+    try:
+        return dns.name.from_text(name).to_text()
+    except dns.exception.DNSException as e:
+        raise DNSNameError(name, e) from None
