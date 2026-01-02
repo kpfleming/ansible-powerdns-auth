07c53f9be6a2f561be40084714e52eed32c5294c
Kevin P. Fleming <kevin@km6g.us>
2026-01-02 11:45:29 -0500

    WIP

--- workflow-support/run_test_playbooks.sh
+++ workflow-support/run_test_playbooks.sh
@@ -2,7 +2,7 @@
 
 set -e # in order to fail on any playbook
 
-ansible-playbook -i localhost, workflow-support/ansible-"${1}"/test-tsigkey.yml
+ansible-playbook -vvv -i localhost, workflow-support/ansible-"${1}"/test-tsigkey.yml
 ansible-playbook -i localhost, workflow-support/ansible-"${1}"/test-zone.yml
 ansible-playbook -i localhost, workflow-support/ansible-"${1}"/test-zone-issue-136.yml
 ansible-playbook -i localhost, workflow-support/ansible-"${1}"/test-zone-rrset.yml
